8e506fb964317488576a179d5f2921b1e04fe8c7
Yan <jan.kozhyn@gmail.com>
2023-03-14 14:22:50 +0200

    Transform.Translate()

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -12,6 +12,7 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
+        transform.Rotate(0,0,0.01f);
+        transform.Translate(0, 0.01f, 0);
     }
 }
