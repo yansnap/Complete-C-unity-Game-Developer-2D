dafba8feb96d7fc4c6475aaaa897e16fb1880d61
Yan <jan.kozhyn@gmail.com>
2023-03-14 14:53:38 +0200

    Using Input.GetAxis()

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] private float steerSpeed = 0.1f;
+    [SerializeField] private float steerSpeed = 1;
     [SerializeField] private float moveSpeed = 0.01f;
-    // Start is called before the first frame update
     void Start()
     {
         
@@ -14,8 +14,10 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        
-        transform.Rotate(0,0,steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Rotate(0,0, steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
