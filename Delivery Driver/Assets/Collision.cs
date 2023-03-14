4b5ce2346af0050586ed9858f4621760110da938
Yan <jan.kozhyn@gmail.com>
2023-03-14 20:26:36 +0200

    Using OnCollisionEnter2D()

---
+++ Delivery Driver/Assets/Collision.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("U collied a object!");
+    }
+}
